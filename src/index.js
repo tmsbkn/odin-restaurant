ff89af00e39f1ecd98403caaece4c355766ecbdb
tmsbkn <54986052+tmsbkn@users.noreply.github.com>
2026-05-11 17:23:04 -0400

    Create functions to load and unload page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './style.css';
 import './reset.css';
+import './loadHome.js';
+
+const content = document.querySelector('#content');
+
+const loadPage = (page) => {
+   unloadPage();
+   page.forEach((element) => {
+      content.appendChild(element);
+   });
+};
+
+const unloadPage = () => {
+   content.innerHTML = '';
+};
 console.log("I'm running!");
