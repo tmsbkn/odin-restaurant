6ed92e514d5143eca37ce9017b85b45acbc06a07
tmsbkn <54986052+tmsbkn@users.noreply.github.com>
2026-05-11 18:02:36 -0400

    create loadhome.js

---
+++ src/loadHome.js
@@ -0,0 +1,15 @@
+const home = document.createElement('div');
+home.setAttribute('id', 'home');
+
+let hero = document.createElement('div');
+
+hero.className = 'hero';
+export default home;
+//  <div id="content" data-page="home">
+//      <div class="hero">
+//         <span class="hero-text">123 Maple Park Blvd, Mapleville, ON</span>
+//         <h1 class="hero-title">The Wipes and Laps Pub</h1>
+//         <span class="hero-text">EST 2015</span>
+//      </div>
+//      <div class="hero-img"></div>
+//   </div>
