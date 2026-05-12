33dd1374d3593feba4604f67669b7be6ea1a9f6f
tmsbkn <54986052+tmsbkn@users.noreply.github.com>
2026-05-11 20:50:52 -0400

    Rewrite menu

--- src/menu.js
+++ src/menu.js
@@ -1,21 +1,40 @@
 import newEl from './elementClass.js';
+import menuItems from './menuItems.json' assert { type: 'json' };
+const menu = newEl('div', 'main', 'menu');
 
-const menu = newEl('div', null, 'home');
+const hero = newEl('div', 'hero');
 
-const hero = newEl('div', 'hero', null);
-
-const heroTitle = newEl('h1', 'hero-title', null);
-heroTitle.textContent = 'The Wipes and Laps Pub';
+const heroTitle = newEl('h1', 'hero-title');
+heroTitle.textContent = menuItems.name;
 
 const address = newEl('span', 'hero-text');
-address.textContent = '123 Maple Park Blvd, Mapleville, ON';
+address.textContent = menuItems.address;
 
 const established = newEl('span', 'hero-text');
 established.textContent = 'EST 2015';
 
 hero.append(address, heroTitle, established);
 let heroImg = newEl('div', 'hero-img');
+const menuSection = newEl('section');
 
-menu.append(hero, heroImg);
+menuItems.items.forEach((item, index) => {
+   const menuItem = newEl('div', 'menu-item', item.id);
+   const itemTitle = newEl('h3', 'item-title');
+   itemTitle.innerText = item.name;
+   const itemPrice = newEl('span', 'price');
+   itemPrice.innerText = item.price;
+   itemTitle.append(' ', itemPrice);
+   const itemDescription = newEl('p', 'item-description');
+   itemDescription.innerText = item.description;
+   const itemAddon = newEl('p', 'add-on');
+   itemAddon.innerText = item.addOn;
+   const itemAddonPrice = newEl('span', 'add-on-price');
+   itemAddonPrice.innerText = item.addOnPrice;
+   itemAddon.append(' ', itemAddonPrice);
+
+   menuItem.append(itemTitle, itemDescription, itemAddon);
+   menuSection.append(menuItem);
+});
+menu.append(hero, menuSection);
 
 export default menu;
